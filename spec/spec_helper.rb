b20aa391209a6a462d17fc37edf521a277521789
Harisankar P S <mailme@hsps.in>
2018-03-07 12:56:25 +0530

    Setup Database Cleaner

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -93,4 +93,17 @@ RSpec.configure do |config|
   # as the one that triggered the failure.
   Kernel.srand config.seed
 =end
+
+  # Database Cleaner
+
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.around(:each) do |example|
+    DatabaseCleaner.cleaning do
+      example.run
+    end
+  end
 end
