4bff70403c10dbec4d10b9459a395ed7b7ebb65e
Aboobacker MK <aboobackervyd@gmail.com>
2018-03-07 17:20:39 +0530

    Fixed Db cleaner configuration

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -94,16 +94,4 @@ RSpec.configure do |config|
   Kernel.srand config.seed
 =end
 
-  # Database Cleaner
-
-  config.before(:suite) do
-    DatabaseCleaner.strategy = :transaction
-    DatabaseCleaner.clean_with(:truncation)
-  end
-
-  config.around(:each) do |example|
-    DatabaseCleaner.cleaning do
-      example.run
-    end
-  end
 end
