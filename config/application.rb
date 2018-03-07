27550f0c96eec74ec2b5d12f7e11ffed0462b8f4
Harisankar P S <mailme@hsps.in>
2018-03-07 09:18:23 +0530

    Customize rails generator
    To avoid stylesheet and javascript.
    Avoid fixtures
    Set template engine as HAML
    Orm as active_record

--- config/application.rb
+++ config/application.rb
@@ -14,5 +14,14 @@ module Starter
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+
+    config.generators do |g|
+      g.orm :active_record
+      g.template_engine :haml
+      g.test_framework :rspec, fixture: false
+      g.stylesheets false
+      g.javascripts false
+    end
   end
 end
